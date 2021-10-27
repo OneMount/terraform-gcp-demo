9a5e933854e85fc3d7a7d0e67a69fe884be78c9f
Văn Anh (Cloud) <anhbv2@onemount.com>
2021-10-27 17:44:23 +0700

    add readme

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "gcs" {
-    bucket      = "om-gcp-tf-state" # Bucket được tạo ở bước trên
+    bucket      = "gcp-tf-state" # Bucket được tạo ở bước trên
     impersonate_service_account = "resources-creator@yourproject.iam.gserviceaccount.com" 
     #service account được tạo ở bước trên 
     # service account này sẽ đọc bucket và ghi state lên bucket
